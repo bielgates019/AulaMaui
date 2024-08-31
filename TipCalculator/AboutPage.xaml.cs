219ac563faf8ab2605c798172ac03dbd179da15d
bielgates019 <gmellos@outlook.com>
2024-08-30 22:47:40 -0300

    Adicionei o codigo

---
+++ TipCalculator/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace TipCalculator;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+        InitializeComponent();
+	}
+}
---
+++ TipCalculator/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace TipCalculator
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+
+        }
+    }
+}
